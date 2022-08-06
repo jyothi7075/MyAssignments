8220ec795f41e1a2a8bed983106915e28264bf38
jyothi7075 <110726403+jyothi7075@users.noreply.github.com>
2022-08-06 18:36:30 +0530

    Add files via upload
    Day1

---
+++ day1/Car.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class Car {
+	
+ boolean isEmpty = false;
+ byte series = 127;
+ char number = 't';
+ short discountPrice = 32000;
+ int a = 5678;
+ long l = 89839898202l;
+ float f = 4.4827992f;
+ double d = 68.2836573;
+ 
+ String name = "jyothi";
+}
---
+++ day1/TwoWheeler.java
@@ -0,0 +1,30 @@
+package week1.day1;
+
+public class TwoWheeler {
+
+	int noOfWheels=2;
+	short noOfMirrors=4;
+	long chasisNumber=13672899898398l;
+	boolean isPunctured=false;
+	String bikeName="Honda";
+	double runningKM=8656.94949;
+
+	public static void main(String[] args) {
+		TwoWheeler tw = new TwoWheeler();
+		System.out.println(tw.noOfWheels);
+		System.out.println(tw.noOfMirrors);
+		System.out.println(tw.chasisNumber);
+        System.out.println(tw.isPunctured);
+        System.out.println(tw.bikeName);
+        System.out.println(tw.runningKM);
+	}
+
+
+
+
+
+
+
+
+
+}
