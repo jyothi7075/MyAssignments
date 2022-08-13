3981885addc76219671fde21ef5fb34dcaa7dbe1
jyothi7075 <110726403+jyothi7075@users.noreply.github.com>
2022-08-13 12:04:43 +0530

    Add files via upload

---
+++ day2/PrintOddNumber.java
@@ -0,0 +1,17 @@
+package week1.day2;
+
+public class PrintOddNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int i=51; i<=77; i++)
+		{
+		if(i%2 == 1) {
+			System.out.println(i);
+		}
+		}
+
+	}
+
+}
