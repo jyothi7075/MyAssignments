20f838806096798bf52266974f34bdca2077ff47
jyothi7075 <110726403+jyothi7075@users.noreply.github.com>
2022-08-20 20:29:57 +0530

    Add files via upload

---
+++ day1/BankInfo.java
@@ -0,0 +1,15 @@
+package week3.day1;
+
+public class BankInfo {
+	public void saving() {
+		System.out.println("savings amount is 2 lakhs");
+	}
+	public void fixed() {
+		System.out.println("Fixed amount is 3 lakhs");
+	}
+	public int deposit() {
+		//System.out.println("Deposit amount is 4 lakhs");
+		return 500000;
+	}
+
+}
