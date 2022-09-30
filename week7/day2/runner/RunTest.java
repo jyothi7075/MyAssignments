cd6d5ce70c2bb2b0e77683e3357467ae84ffe986
jyothi7075 <110726403+jyothi7075@users.noreply.github.com>
2022-09-30 11:47:50 -0700

    Delete week7/day2 directory

--- week7/day2/runner/RunTest.java
+++
@@ -1,9 +0,0 @@
-package week7.day2.runner;
-
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-
-@CucumberOptions(features="src/test/java/week7/day2/feature/leaftapsCreateLead.feature", glue="week7/day2/stepdef")
-public class RunTest extends AbstractTestNGCucumberTests{
-
-}
